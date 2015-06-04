5249342bdb9c3ce1c525e1215bff53139197b06e
Scott Smith <mail@ScottHSmith.com>
2015-06-03 17:28:02 -0700

    Add comments, improve formatting

--- assets/scripts/portfolio.js
+++ assets/scripts/portfolio.js
@@ -1,14 +1,16 @@
 docReady( function() {
-  var grid = document.querySelector('.site-content');
-
-  var msnry = new Masonry( grid, {
-    itemSelector: '.post',
-    transitionDuration: 0
-  });
-
-  imagesLoaded( grid, function() {
-    // layout Masonry after each image loads
-    msnry.layout();
-  });
+	
+	var grid = document.querySelector('.site-content');
+	
+	// Setup Masonry.
+	var msnry = new Masonry( grid, {
+		itemSelector: '.post',
+		transitionDuration: 0
+	});
+	
+	imagesLoaded( grid, function() {
+		// Layout Masonry after each image loads.
+		msnry.layout();
+	});
 
 });
