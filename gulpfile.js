88e5881d3ca1b704738801544debdb1c622c1dfb
Scott Smith <mail@scotthsmith.com>
2016-10-19 19:34:49 -0400

    Removed scripts.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,11 @@
+'use strict';
+
 var gulp       = require('gulp'),
 	postcss    = require('gulp-postcss'),
-	concat     = require('gulp-concat'),
-	uglify     = require('gulp-uglify'),
 	sourcemaps = require('gulp-sourcemaps');
 
 var paths = {
-	styles: ['assets/styles/*.css'],
-	indexScripts: ['node_modules/imagesloaded/imagesloaded.pkgd.js', 'node_modules/masonry-layout/dist/masonry.pkgd.js', 'assets/scripts/portfolio.js'],
+	styles: ['assets/styles/*.css']
 };
 
 gulp.task(function styles() {
@@ -25,27 +24,17 @@ gulp.task(function styles() {
 		.pipe(gulp.dest('assets/styles/build/'));
 });
 
-gulp.task(function indexScripts() {
-	return gulp.src(paths.indexScripts)
-		.pipe(sourcemaps.init())
-			.pipe(concat('portfolio.js'))
-			.pipe(uglify())
-		.pipe(sourcemaps.write('./'))
-		.pipe(gulp.dest('assets/scripts/build/'));
-});
-
 gulp.task(function watch() {
 	gulp.watch(paths.styles, gulp.series('styles'));
-	gulp.watch(paths.indexScripts, gulp.series('indexScripts'));
 });
 
 // Workflows
 // $ gulp: Builds, prefixes, and minifies CSS files; concencates and minifies JS files; watches for changes. The works.
-gulp.task('default', gulp.parallel('styles', 'indexScripts', 'watch', function(done) {
+gulp.task('default', gulp.parallel('styles', 'watch', function(done) {
 	done();
 }));
 
 // $ gulp build: Builds, prefixes, and minifies CSS files; concencates and minifies JS files. For deployments.
-gulp.task('build', gulp.parallel('styles', 'indexScripts', function(done) {
+gulp.task('build', gulp.parallel('styles', function(done) {
 	done();
 }));
