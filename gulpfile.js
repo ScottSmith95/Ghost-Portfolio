13ff4ebda291e027f51d09ef82b33c9ca74e1a2d
Scott Smith <mail@scotthsmith.com>
2016-10-25 03:29:03 -0400

    Added toggle for larger More Project layout.

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,12 @@
 
 var gulp       = require('gulp'),
 	postcss    = require('gulp-postcss'),
+	uglify     = require('gulp-uglify'),
 	sourcemaps = require('gulp-sourcemaps');
 
 var paths = {
-	styles: ['assets/styles/*.css']
+	styles: ['assets/styles/*.css'],
+	scripts: ['assets/scipts/*.js']
 };
 
 gulp.task(function styles() {
@@ -24,17 +26,26 @@ gulp.task(function styles() {
 		.pipe(gulp.dest('assets/styles/build/'));
 });
 
+gulp.task(function scripts() {
+	return gulp.src(paths.scripts)
+		.pipe(sourcemaps.init())
+			.pipe(uglify())
+		.pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest('assets/scripts/build/'));
+});
+
 gulp.task(function watch() {
 	gulp.watch(paths.styles, gulp.series('styles'));
+	gulp.watch(paths.scripts, gulp.series('scripts'));
 });
 
 // Workflows
 // $ gulp: Builds, prefixes, and minifies CSS files; concencates and minifies JS files; watches for changes. The works.
-gulp.task('default', gulp.parallel('styles', 'watch', function(done) {
+gulp.task('default', gulp.parallel('styles', 'scripts', 'watch', function(done) {
 	done();
 }));
 
 // $ gulp build: Builds, prefixes, and minifies CSS files; concencates and minifies JS files. For deployments.
-gulp.task('build', gulp.parallel('styles', function(done) {
+gulp.task('build', gulp.parallel('styles', 'scripts', function(done) {
 	done();
 }));
