06d17f13615909d5a85c81452f973a2c557b2c13
Scott Smith <mail@ScottHSmith.com>
2017-06-22 23:38:45 +0000

    Improved code quality and task naming.

--- gulpfile.js
+++ gulpfile.js
@@ -6,46 +6,60 @@ var gulp       = require('gulp'),
 	sourcemaps = require('gulp-sourcemaps');
 
 var paths = {
-	styles: ['assets/styles/*.css'],
-	scripts: ['assets/scipts/*.js']
+	styles: {
+		src: 'assets/styles/portfolio.css',
+		dest: 'assets/styles/build/',
+		watch: 'assets/styles/*.css'
+	},
+	scripts: {
+		src: 'assets/scipts/*.js',
+		dest: 'assets/scripts/build/'
+	}
 };
 
-gulp.task(function styles() {
-	var processors = [
-		require('postcss-import'),
-		require('postcss-nested'),
-		require('postcss-custom-properties'),
-		require('css-mqpacker')({sort: true}),
-		require('autoprefixer'),
-		require('cssnano')({autoprefixer: false})
-    ];
-	return gulp.src('assets/styles/portfolio.css')
+var processors = [
+	require('postcss-import'),
+	require('postcss-nested'),
+	require('postcss-custom-properties'),
+	require('css-mqpacker')({sort: true}),
+	require('autoprefixer'),
+	require('cssnano')({autoprefixer: false})
+];
+
+function styles() {
+	return gulp.src(paths.styles.src)
 		.pipe(sourcemaps.init())
 			.pipe(postcss(processors))
 		.pipe(sourcemaps.write('./'))
-		.pipe(gulp.dest('assets/styles/build/'));
-});
+		.pipe(gulp.dest(paths.styles.dest));
+}
 
-gulp.task(function scripts() {
-	return gulp.src(paths.scripts)
+function scripts() {
+	return gulp.src(paths.scripts.src)
 		.pipe(sourcemaps.init())
 			.pipe(uglify())
 		.pipe(sourcemaps.write('./'))
-		.pipe(gulp.dest('assets/scripts/build/'));
-});
+		.pipe(gulp.dest(paths.scripts.dest));
+}
 
-gulp.task(function watch() {
-	gulp.watch(paths.styles, gulp.series('styles'));
-	gulp.watch(paths.scripts, gulp.series('scripts'));
-});
+function watch() {
+	gulp.watch(paths.styles.watch, styles);
+	gulp.watch(paths.scripts.src, scripts);
+}
 
 // Workflows
 // $ gulp: Builds, prefixes, and minifies CSS files; concencates and minifies JS files; watches for changes. The works.
-gulp.task('default', gulp.parallel('styles', 'scripts', 'watch', function(done) {
-	done();
-}));
+var defaultTask = gulp.parallel(styles, scripts, watch);
 
 // $ gulp build: Builds, prefixes, and minifies CSS files; concencates and minifies JS files. For deployments.
-gulp.task('build', gulp.parallel('styles', 'scripts', function(done) {
-	done();
-}));
+var buildTask = gulp.parallel(styles, scripts);
+
+// Exports
+// Externalise individual tasks.
+exports.styles = styles;
+exports.scripts = scripts;
+exports.watch = watch;
+
+// Externalise Workflows.
+exports.build = buildTask;
+exports.default = defaultTask;
